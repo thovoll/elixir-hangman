9a55e3f4584b128fa5691c452683a3a4ff2c4da8
Thomas Vollmer <34384788+thovoll@users.noreply.github.com>
2018-09-04 22:09:04 -0700

    Moved to other projects.

--- lib/hangman.ex
+++ lib/hangman.ex
@@ -1,11 +1,7 @@
-defmodule Dictionary do
-  def word_list do
-    "assets/words.txt"
-    |> File.read!()
-    |> String.split(~r/\n/)
+defmodule Hangman do
+
+  def hello_world do
+    IO.puts "hello, world!"
   end
-  def random_word do
-    word_list()
-    |> Enum.random()
-  end
+  
 end
--- lib/patternmatching.ex
+++
@@ -1,8 +0,0 @@
-defmodule PatternMatching do
-    def swap({ a, b }) do
-        { b, a }
-    end
-    def same?({ a, a }), do: true
-    def same?({ a, b }), do: false
-end
-  
