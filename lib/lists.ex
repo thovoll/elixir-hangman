be5e25b64667025e717962764b1fd2c730f79235
Thomas Vollmer <34384788+thovoll@users.noreply.github.com>
2018-09-04 21:50:46 -0700

    Summing pairs and determining even length.

--- lib/lists.ex
+++ lib/lists.ex
@@ -17,4 +17,10 @@ defmodule Lists do
     def map([], func), do: []
     def map([h | t], func), do: [ func.(h) | map(t, func) ]
 
+    def sum_pairs([]), do: []
+    def sum_pairs([ h1, h2 | t]), do: [ h1 + h2 | sum_pairs(t) ]
+
+    def even_length?([ h1, h2 | []]), do: true
+    def even_length?([ h1 | []]), do: false
+    def even_length?([ h1, h2 | t]), do: even_length?(t)
 end
