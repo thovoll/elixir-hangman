b6e8d128584157b9559b03aea2dedb8be515c503
Thomas Vollmer <34384788+thovoll@users.noreply.github.com>
2018-09-08 15:38:52 -0700

    Move tally concern to public API level.

--- lib/hangman.ex
+++ lib/hangman.ex
@@ -4,7 +4,11 @@ defmodule Hangman do
   alias Hangman.Game
 
   defdelegate new_game(),             to: Game
-  defdelegate make_move(game, guess), to: Game
   defdelegate tally(game),            to: Game
+
+  def make_move(game, guess) do
+    game = Game.make_move(game, guess)
+    { game, tally(game) }    
+  end
 
 end
--- lib/hangman/game.ex
+++ lib/hangman/game.ex
@@ -19,12 +19,11 @@ defmodule Hangman.Game do
     end
 
     def make_move(game = %{ game_state: state }, _guess) when state in [:won, :lost] do
-        { game, tally(game) }
+        game
     end
 
     def make_move(game, guess) do
-        game = accept_move(game, guess, MapSet.member?(game.used, guess))
-        { game, tally(game) }
+        accept_move(game, guess, MapSet.member?(game.used, guess))
     end
    
     def tally(game) do
