3a3d34c680f626d8d0ffd182ed4fd22acb5b3881
Thomas Vollmer <34384788+thovoll@users.noreply.github.com>
2018-09-08 15:25:00 -0700

    Support tally.

--- lib/hangman/game.ex
+++ lib/hangman/game.ex
@@ -55,8 +55,20 @@ defmodule Hangman.Game do
     end
 
     def tally(game) do
-        123
+        %{
+            game_state: game.game_state,
+            turns_left: game.turns_left,
+            letters:    game.letters |> reveal_guessed(game.used)
+        }
     end
+
+    def reveal_guessed(letters, used) do
+        letters
+        |> Enum.map(fn letter -> reveal_letter(letter, MapSet.member?(used, letter)) end)
+    end
+
+    def reveal_letter(letter, _in_word = true), do: letter
+    def reveal_letter(letter, _not_in_word),    do: "_"
 
     def maybe_won?(true), do: :won
     def maybe_won?(_), do: :good_guess
