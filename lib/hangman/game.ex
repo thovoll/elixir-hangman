5c3f6cd31b0ca8af368a492ef7b01eb545434c6c
Thomas Vollmer <34384788+thovoll@users.noreply.github.com>
2018-09-08 08:11:12 -0700

    Bad guess and lost.

--- lib/hangman/game.ex
+++ lib/hangman/game.ex
@@ -43,8 +43,15 @@ defmodule Hangman.Game do
         Map.put(game, :game_state, new_state)
     end
 
-    def score_guess(game, _not_good_guess) do
-        game
+    def score_guess(game = %{ turns_left: 1 }, _not_good_guess) do
+        Map.put(game, :game_state, :lost)
+    end
+
+    def score_guess(game = %{ turns_left: turns_left }, _not_good_guess) do
+        %{ game | 
+            game_state: :bad_guess,
+            turns_left: turns_left - 1
+        }
     end
 
     def tally(game) do
--- test/game_test.exs
+++ test/game_test.exs
@@ -63,4 +63,35 @@ defmodule GameTest do
         assert game.turns_left == 7
     end
 
+    test "a bad guess is recognized" do
+        game = Game.new_game("wibble")
+        { game, _tally } = Game.make_move(game, "x")
+        assert game.game_state == :bad_guess
+        assert game.turns_left == 6
+    end
+
+    test "lost game is recognized" do
+        game = Game.new_game("w")
+        { game, _tally } = Game.make_move(game, "a")
+        assert game.game_state == :bad_guess
+        assert game.turns_left == 6
+        { game, _tally } = Game.make_move(game, "b")
+        assert game.game_state == :bad_guess
+        assert game.turns_left == 5
+        { game, _tally } = Game.make_move(game, "c")
+        assert game.game_state == :bad_guess
+        assert game.turns_left == 4
+        { game, _tally } = Game.make_move(game, "d")
+        assert game.game_state == :bad_guess
+        assert game.turns_left == 3
+        { game, _tally } = Game.make_move(game, "e")
+        assert game.game_state == :bad_guess
+        assert game.turns_left == 2
+        { game, _tally } = Game.make_move(game, "f")
+        assert game.game_state == :bad_guess
+        assert game.turns_left == 1
+        { game, _tally } = Game.make_move(game, "g")
+        assert game.game_state == :lost
+    end
+
 end
