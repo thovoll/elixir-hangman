467166d1d5468c4a0568c8cdfce79ccc66c06577
Thomas Vollmer <34384788+thovoll@users.noreply.github.com>
2018-09-08 08:00:54 -0700

    Good moves.

--- lib/hangman/game.ex
+++ lib/hangman/game.ex
@@ -9,8 +9,12 @@ defmodule Hangman.Game do
     )
 
     def new_game() do
+        new_game(Dictionary.random_word)
+    end
+
+    def new_game(word) do
         %Hangman.Game{ 
-            letters: Dictionary.random_word |> String.codepoints  
+            letters: word |> String.codepoints  
         }
     end
 
@@ -29,11 +33,26 @@ defmodule Hangman.Game do
 
     def accept_move(game, guess, _already_guessed) do
         Map.put(game, :used, MapSet.put(game.used, guess))
+        |> score_guess(Enum.member?(game.letters, guess))
+    end
+
+    def score_guess(game, _good_guess = true) do
+        new_state = MapSet.new(game.letters)
+                    |> MapSet.subset?(game.used)
+                    |> maybe_won?
+        Map.put(game, :game_state, new_state)
+    end
+
+    def score_guess(game, _not_good_guess) do
+        game
     end
 
     def tally(game) do
         123
     end
+
+    def maybe_won?(true), do: :won
+    def maybe_won?(_), do: :good_guess
 
 end
   
--- test/game_test.exs
+++ test/game_test.exs
@@ -16,7 +16,7 @@ defmodule GameTest do
         assert String.match?(s, ~r/[a-z]+/)
     end
 
-    test "state isn't change for :won or :lost game" do
+    test "state isn't change for won or lost game" do
         for state <- [ :won, :lost] do
             game = Game.new_game() |> Map.put(:game_state, state)
             assert { ^game, _ } = Game.make_move(game, "x")
@@ -35,6 +35,32 @@ defmodule GameTest do
         assert game.game_state != :already_used
         { game, _tally } = Game.make_move(game, "x")
         assert game.game_state == :already_used
+    end
+
+    test "a good guess is recognized" do
+        game = Game.new_game("wibble")
+        { game, _tally } = Game.make_move(game, "w")
+        assert game.game_state == :good_guess
+        assert game.turns_left == 7
+    end
+
+    test "a guessed word is a won game" do
+        game = Game.new_game("wibble")
+        { game, _tally } = Game.make_move(game, "w")
+        assert game.game_state == :good_guess
+        assert game.turns_left == 7
+        { game, _tally } = Game.make_move(game, "i")
+        assert game.game_state == :good_guess
+        assert game.turns_left == 7
+        { game, _tally } = Game.make_move(game, "b")
+        assert game.game_state == :good_guess
+        assert game.turns_left == 7
+        { game, _tally } = Game.make_move(game, "l")
+        assert game.game_state == :good_guess
+        assert game.turns_left == 7
+        { game, _tally } = Game.make_move(game, "e")
+        assert game.game_state == :won
+        assert game.turns_left == 7
     end
 
 end
