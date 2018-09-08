db86ab59dd618eaafd63d0c42e295c8efb866c13
Thomas Vollmer <34384788+thovoll@users.noreply.github.com>
2018-09-08 06:13:15 -0700

    Add some tests.

--- lib/hangman/game.ex
+++ lib/hangman/game.ex
@@ -13,5 +13,13 @@ defmodule Hangman.Game do
         }
     end
 
+    def make_move(game = %{ game_state: state }, _guess) when state in [:won, :lost] do
+        { game, tally(game) }
+    end
+
+    def tally(game) do
+        123
+    end
+
 end
   
---
+++ test/game_test.exs
@@ -0,0 +1,27 @@
+defmodule GameTest do
+    use ExUnit.Case
+
+    alias Hangman.Game
+
+    test "new_game returns structure" do
+        game = Game.new_game()
+        assert game.turns_left == 7
+        assert game.game_state == :initializing
+        assert length(game.letters) > 0
+    end
+
+    test "new_game returns lower case letters" do
+        game = Game.new_game()
+        s = List.to_string(game.letters)
+        assert String.match?(s, ~r/[a-z]+/)
+    end
+
+    test "state isn't change for :won or :lost game" do
+        for state <- [ :won, :lost] do
+            game = Game.new_game() |> Map.put(:game_state, state)
+            assert { ^game, _ } = Game.make_move(game, "x")
+        end
+    end
+
+end
+  
