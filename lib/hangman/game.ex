e51d2b436bb482eb0fcd5341eed6355afac21a01
Thomas Vollmer <34384788+thovoll@users.noreply.github.com>
2018-09-14 20:41:41 -0700

    Use struct aliases.

--- lib/hangman/game.ex
+++ lib/hangman/game.ex
@@ -7,26 +7,28 @@ defmodule Hangman.Game do
     used: MapSet.new()
   )
 
+  alias Hangman.{Game, Tally}
+
   def new_game() do
     new_game(Dictionary.random_word())
   end
 
   def new_game(word) do
-    %Hangman.Game{
+    %Game{
       letters: word |> String.codepoints()
     }
   end
 
-  def make_move(game = %{game_state: state}, _guess) when state in [:won, :lost] do
+  def make_move(game = %Game{game_state: state}, _guess) when state in [:won, :lost] do
     game
   end
 
-  def make_move(game, guess) do
+  def make_move(game = %Game{}, guess) do
     accept_move(game, guess, MapSet.member?(game.used, guess))
   end
 
-  def tally(game) do
-    %Hangman.Tally{
+  def tally(game = %Game{}) do
+    %Tally{
       game_state: game.game_state,
       turns_left: game.turns_left,
       letters: game.letters |> reveal_guessed(game.used),
@@ -36,29 +38,28 @@ defmodule Hangman.Game do
 
   ##############################################################################
 
-  defp accept_move(game, _guess, _already_guessed = true) do
+  defp accept_move(game = %Game{}, _guess, _already_guessed = true) do
     Map.put(game, :game_state, :already_used)
   end
 
-  defp accept_move(game, guess, _already_guessed) do
+  defp accept_move(game = %Game{}, guess, _already_guessed) do
     Map.put(game, :used, MapSet.put(game.used, guess))
     |> score_guess(Enum.member?(game.letters, guess))
   end
 
-  defp score_guess(game, _good_guess = true) do
+  defp score_guess(game = %Game{}, _good_guess = true) do
     new_state =
       MapSet.new(game.letters)
       |> MapSet.subset?(game.used)
       |> maybe_won?
-
     Map.put(game, :game_state, new_state)
   end
 
-  defp score_guess(game = %{turns_left: 1}, _not_good_guess) do
+  defp score_guess(game = %Game{turns_left: 1}, _not_good_guess) do
     Map.put(game, :game_state, :lost)
   end
 
-  defp score_guess(game = %{turns_left: turns_left}, _not_good_guess) do
+  defp score_guess(game = %Game{turns_left: turns_left}, _not_good_guess) do
     %{game | game_state: :bad_guess, turns_left: turns_left - 1}
   end
 
